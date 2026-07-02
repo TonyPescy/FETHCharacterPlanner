a15005cf9a7736578c50474c89981619e7ea594e
TonyPescy <tpescy16@gmail.com>
2026-07-02 16:01:17 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# FETHCharacterPlanner
+A Flutter project that will help end users plan their Fire Emblem Three Houses character builds better.
---
+++ feth_character_planner/android/app/src/main/kotlin/com/example/feth_character_planner/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.feth_character_planner
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
