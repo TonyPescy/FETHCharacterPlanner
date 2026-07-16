fd7c688ebae10d8e110771b02263df4db64b3db3
TonyPescy <tpescy16@gmail.com>
2026-07-16 13:44:50 -0400

    Stats functions and getters laid out for implementation.

--- feth_character_planner/lib/models/stats.dart
+++ feth_character_planner/lib/models/stats.dart
@@ -1,3 +1,5 @@
+import 'dart:nativewrappers/_internal/vm/lib/ffi_native_type_patch.dart';
+
 class Stats {
   // Constant stat abbreviations in order of apperance - Used for displaying and more
   static const statsList = ["HP", "Str", "Mag", "Dex", "Spd", "Lck", "Def", "Res", "Cha"];
@@ -9,8 +11,10 @@ class Stats {
   //      Map classLevels
   //          name: Class name - Used for finding class growths
   //          levels: levels spent in class - Used for calculating how long someone is in a class
-  // Returns: List lowLuckStats - List of all calculated stats in order
-  List<int> getLowLuckStats() {
+  // Returns: Map lowLuckStats - List of all calculated stats in order
+  //            stat: stat name
+  //            statVal: how much of a stat the char has
+  List<int> getLowLuckStats(String characterID, Map classLevels) {
 
     
 
@@ -18,10 +22,31 @@ class Stats {
     List<int> newList = [0, 2];
     return newList;
   }
+  // Get Low Luck Stats End
 
+  // Get Character Growth Rates Start
+  Map<String, Float> getCharacterGrowthRates(String characterID) {
+    throw UnimplementedError("NOT IMPLEMENTED YET!");
+  }
+  // Get Character Growth Rates End
 
+  // Get Class Growth Rates Start
+  Map<String, Float> getClassGrowthRates(String classID) {
+    throw UnimplementedError("NOT IMPLEMENTED YET!");
+  }
+  // Get Class Growth Rates End
 
+  // Get Character Base Stats Start
+  Map<String, Float> getCharacterBaseStats(String characterID) {
+    throw UnimplementedError("NOT IMPLEMENTED YET!");
+  }
+  // Get Character Base Stats End
 
+  // Get Combined Growth Rates Start
+  Map<String, Float> getCombinedGrowthRates(String characterID, String classID) {
+    throw UnimplementedError("NOT IMPLEMENTED YET!");
+  }
+  // Get Combined Growth Rates End
 
 
 
