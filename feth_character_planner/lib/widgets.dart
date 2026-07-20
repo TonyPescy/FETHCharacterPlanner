f739a71685a2966c5c13d27c9ea879c051c29cb0
TonyPescy <tpescy16@gmail.com>
2026-07-20 09:51:29 -0400

    Start building single chaarcter plan display areas in MyPlans

--- feth_character_planner/lib/widgets.dart
+++ feth_character_planner/lib/widgets.dart
@@ -1,3 +1,4 @@
+import 'package:feth_character_planner/models/stats.dart';
 import 'package:flutter/material.dart';
 import 'package:feth_character_planner/themes.dart';
 import 'package:feth_character_planner/pages.dart';
@@ -423,7 +424,7 @@ class PlanDisplayCard extends StatefulWidget {
                         ),
 
                         //const SizedBox(height: 12),
-
+                        // Content - Unit Names
                         Expanded(
                           child: Scrollbar(
                             controller: _scrollController,
@@ -492,149 +493,158 @@ class PlanDisplayCard extends StatefulWidget {
       );
       } else if (widget.plan.type == "character") {
         // Will need to calculate the non-RNG stats as the AVG of all 4 are shown here
-        final stats = widget.plan.characters[0].id
+        // Get RNG stats as stats
+        final stats = widget.plan.characters[0].stats;
+
+        // Get character classes from plan
+        final classes = widget.plan.characters[0].classes;
+        
+        
+
+        // Calculate low-luck stats for character
+        final lowLuckStats = Stats().getLowLuckStats(widget.plan.id, classes);
         return MouseRegion(
-        cursor: SystemMouseCursors.click,
-        onEnter: (_) => setState(() => hovered = true),
-        onExit: (_) => setState(() => hovered = false),
+          cursor: SystemMouseCursors.click,
+          onEnter: (_) => setState(() => hovered = true),
+          onExit: (_) => setState(() => hovered = false),
 
-        child: LayoutBuilder( 
-          builder: (context, constraints) {
-            // Columns for card names
-            //const columns = 4;
-            // Columns for card stats
+          child: LayoutBuilder( 
+            builder: (context, constraints) {
+              // Columns for card names
+              //const columns = 4;
+              // Columns for card stats
 
-            // Card width based on screen dimensions
-            final cardWidth = MediaQuery.of(context).size.width * 0.80;
-            final cardHeight = MediaQuery.of(context).size.height * 0.20; // 20%
-            // Sizing based on card width
-            //final imageWidth = cardWidth * 0.10;
-            //final buttonWidth = cardWidth * 0.10;
-            final imageSize = cardWidth * 0.08;
-            final iconSize = imageSize * 0.33; // Same as image but seperated into 3 parts
+              // Card width based on screen dimensions
+              final cardWidth = MediaQuery.of(context).size.width * 0.80;
+              final cardHeight = MediaQuery.of(context).size.height * 0.20; // 20%
+              // Sizing based on card width
+              //final imageWidth = cardWidth * 0.10;
+              //final buttonWidth = cardWidth * 0.10;
+              final imageSize = cardWidth * 0.08;
+              final iconSize = imageSize * 0.33; // Same as image but seperated into 3 parts
 
-            // Will be filled using data that is read from single JSON file
-            return AnimatedContainer(
-              // Style
-              height: cardHeight,
-              // Hover effects
-              duration: const Duration(milliseconds: 150),
-              transform: Matrix4.translationValues(0, hovered ? -6 : 0, 0), // Lift above ovther cards
-              decoration: BoxDecoration(
-                color: theme.primary,
-                borderRadius: BorderRadius.circular(12),
-                border: Border.all(
-                  color: hovered ? theme.icon : Colors.transparent,
-                  width: 3,
-                ),
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.black.withValues(alpha: hovered ? 0.30 : 0.12),
-                    blurRadius: hovered ? 24 : 8,
-                    spreadRadius: hovered ? 2 : 0,
-                    offset: Offset(0, hovered ? 10 : 4),
+              // Will be filled using data that is read from single JSON file
+              return AnimatedContainer(
+                // Style
+                height: cardHeight,
+                // Hover effects
+                duration: const Duration(milliseconds: 150),
+                transform: Matrix4.translationValues(0, hovered ? -6 : 0, 0), // Lift above ovther cards
+                decoration: BoxDecoration(
+                  color: theme.primary,
+                  borderRadius: BorderRadius.circular(12),
+                  border: Border.all(
+                    color: hovered ? theme.icon : Colors.transparent,
+                    width: 3,
                   ),
-                ],
-              ),
-              child: Row(
-                children: [
-                  // Left Character PFP/House PFP
-                  Expanded(
-                    flex: 1, // NEEDS TO BE REACTIVE
-                    child: IconButton(
-                      iconSize: imageSize, // NEEDS TO BE REACTIVE
-                      icon: const Icon(Icons.add_photo_alternate),  // PLACEHOLDER
-                      onPressed: () {},
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black.withValues(alpha: hovered ? 0.30 : 0.12),
+                      blurRadius: hovered ? 24 : 8,
+                      spreadRadius: hovered ? 2 : 0,
+                      offset: Offset(0, hovered ? 10 : 4),
                     ),
-                  ),
+                  ],
+                ),
+                child: Row(
+                  children: [
+                    // Left Character PFP/House PFP
+                    Expanded(
+                      flex: 1, // NEEDS TO BE REACTIVE
+                      child: IconButton(
+                        iconSize: imageSize, // NEEDS TO BE REACTIVE
+                        icon: const Icon(Icons.add_photo_alternate),  // PLACEHOLDER
+                        onPressed: () {},
+                      ),
+                    ),
 
-                  //const SizedBox(width: 16),
+                    //const SizedBox(width: 16),
 
-                  // Middle - Character Stats or House Units
-                  Expanded(
-                    flex: 8,
-                    // Start changes for character specific stuff
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      // If plan is a house plan
-                      children: [
-                        Text(
-                          widget.plan.name,
-                          style: TextStyle(
-                            fontSize: AppTextSizes.heading(context),
-                            fontWeight: FontWeight.bold,
-                            color: theme.surface,
-                            height: 1.0, // Shorten to make more room for Scrollbar content
-                          ),
-                        ),
-
-                        //const SizedBox(height: 12),
-
-                        Expanded(
-                          child: Scrollbar(
-                            controller: _scrollController,
-                            //thumbVisibility: true,
-                            child: GridView.builder(
-                              controller: _scrollController,
-                              itemCount: members.length,
-                              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                                crossAxisCount: 4,
-                                childAspectRatio: 6,  // Try 8, 6 and more on all screen types
-                              ),
-                              itemBuilder: (context, index) {
-                                return Center(
-                                  child: Text(
-                                    members[index].id,
-                                    textAlign: TextAlign.center,
-                                  ),
-                                );
-                              },
+                    // Middle - Character Stats or House Units
+                    Expanded(
+                      flex: 8,
+                      // Start changes for character specific stuff
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        // Plan Name
+                        children: [
+                          Text(
+                            widget.plan.name,
+                            style: TextStyle(
+                              fontSize: AppTextSizes.heading(context),
+                              fontWeight: FontWeight.bold,
+                              color: theme.surface,
+                              height: 1.0, // Shorten to make more room for Scrollbar content
                             ),
                           ),
-                        ),
-                      ],
+
+                          //const SizedBox(height: 12),
+                          // Content - Unit Stats
+                          Expanded(
+                            child: Scrollbar(
+                              controller: _scrollController,
+                              //thumbVisibility: true,
+                              child: GridView.builder(
+                                controller: _scrollController,
+                                itemCount: Stats.statsList.length,  // Length of stats to be shown
+                                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                                  crossAxisCount: 4,
+                                  childAspectRatio: 6,  // Try 8, 6 and more on all screen types
+                                ),
+                                itemBuilder: (context, index) {
+                                  return Center(
+                                    child: Text(
+                                      avg[index].id,
+                                      textAlign: TextAlign.center,
+                                    ),
+                                  );
+                                },
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
                     ),
-                  ),
 
-                  //const SizedBox(width: 16),
+                    //const SizedBox(width: 16),
 
-                  // Right - Edit, Export, and delete buttons
-                  Expanded( 
-                    flex: 1,
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        IconButton(
-                          icon: const Icon(Icons.edit),
-                          color: theme.icon,
-                          iconSize: iconSize,
-                          tooltip: "Edit",
-                          onPressed: () {},
-                        ),
-                        IconButton(
-                          icon: const Icon(Icons.import_export),
-                          color: theme.icon,
-                          iconSize: iconSize,
-                          tooltip: "Export",
-                          onPressed: () {},
-                        ),
-                        IconButton(
-                          icon: const Icon(Icons.delete),
-                          color: theme.icon,
-                          iconSize: iconSize,
-                          tooltip: "Delete",
-                          onPressed: () {},
-                        ),
-                      ],
+                    // Right - Edit, Export, and delete buttons
+                    Expanded( 
+                      flex: 1,
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          IconButton(
+                            icon: const Icon(Icons.edit),
+                            color: theme.icon,
+                            iconSize: iconSize,
+                            tooltip: "Edit",
+                            onPressed: () {},
+                          ),
+                          IconButton(
+                            icon: const Icon(Icons.import_export),
+                            color: theme.icon,
+                            iconSize: iconSize,
+                            tooltip: "Export",
+                            onPressed: () {},
+                          ),
+                          IconButton(
+                            icon: const Icon(Icons.delete),
+                            color: theme.icon,
+                            iconSize: iconSize,
+                            tooltip: "Delete",
+                            onPressed: () {},
+                          ),
+                        ],
+                      )
                     )
-                  )
-                ]
-              )
-            );
-          }
-        )
-      );
+                  ]
+                )
+              );
+            }
+          )
+        );
 
 
 
